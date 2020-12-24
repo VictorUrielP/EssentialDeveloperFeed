fa6f5b950890418a5f203ae76180d61b6be87618
Uriel Pacheco <victorurielp@gmail.com>
2020-12-23 22:20:17 -0600

    Create FeedItem struct

---
+++ Sources/EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Victor Pacheco on 23/12/20.
+//
+
+import Foundation
+
+public struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
